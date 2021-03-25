c556172540abf0faa9414ba2a46afab27b5bd064
chathurahjm <chathudagr8@yahoo.com>
2021-03-25 21:00:29 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Constants {
+
+        public static final String URL = "https://demoqa.com/automation-practice-form";
+        public static final String Path_TestData = "/Users/chathurajayasinghe/IdeaProjects/cja_sample_test/untitled/Excel/src/testData/";
+        public static final String File_TestData = "Book1.xls";
+
+}
